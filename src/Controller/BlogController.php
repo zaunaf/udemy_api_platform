f192f2c8bf50d814af0bf1f23f5402695fc0d3da
Abah <donny@nufaza.com>
2019-08-27 17:36:00 +0700

    Step 02 - Membuat route menampilkan by id, by slug dan list per-page

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -15,6 +15,44 @@ class BlogController extends AbstractController
 {
 
     /**
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     */
+    public function post($id) {
+        return $this->json(
+            $this->getDoctrine()->getRepository(BlogPost::class)->find($id)
+        );
+    }
+
+    /**
+     * @Route("/post/{slug}", name="blog_by_slug") 
+     */
+    public function postBySlug($slug)
+    {
+        return $this->json(
+            $this->getDoctrine()->getRepository(BlogPost::class)->findBy(['slug' => $slug])
+        );
+    }
+
+    
+    /**
+     * @Route ("/{page}", name="blog_list", defaults={"page": 5}, requirements={"page"="\d+"})
+     */
+    public function list($page = 1, Request $request) {
+        $limit = $request->get('limit', 10);
+        $repository = $this->getDoctrine()->getRepository(BlogPost::class);
+        $items = $repository->findAll();
+        return $this->json(
+            [
+                'page' => $page,
+                'limit' => $limit,
+                'data' => array_map(function(BlogPost $item) {
+                    return $this->generateUrl('blog_by_slug', ['slug' => $item->getSlug()]);
+                }, $items)
+            ]
+        );
+    }
+
+    /**
      * @Route("/add", name="blog_add", methods={"POST"})
      */
     public function add(Request $request)
