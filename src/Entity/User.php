f7f9b2aa072c9dd8cd23c920b779aa1ef74a6193
Abah <donny@nufaza.com>
2019-09-03 03:23:14 +0700

    Step 11 - Menginstall dan Mengkonfigurasi JWT dengan Lexik JWT Auth Bundle

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
